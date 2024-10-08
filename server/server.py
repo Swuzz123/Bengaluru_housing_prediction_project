e4057f14e338edf528eac36f96e95b0470c97a4a
Swuzz123 <ngminhtri21102004@gmail.com>
2024-10-08 10:15:24 +0700

    upload file

---
+++ server/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    data = request.json 
+    total_sqft = float(data['total_sqft'])
+    location = data['location']
+    bhk = int(data['bhk'])
+    bath = int(data['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
